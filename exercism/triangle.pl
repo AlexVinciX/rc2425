fe0d150a9ac8d7954e84ce7df4a09279484d1d25
AlexVinciX <alejandro.hinestrosa@alu.uhu.es>
2025-03-07 13:32:24 +0100

    Create triangle.pl

---
+++ exercism/triangle.pl
@@ -0,0 +1,17 @@
+/* 
+triangle(A, B, C, R).
+ es cierto si R unicifa con "equilateral" y A, B y C corresponden a la longitud de un triangulo equilatero.
+ es cierto si R unifica con "isosceles" y A, B y C corresponden a la longitud de un triangulo isosceles.
+ es cierto si R unifica con "escaleno" y A, B y C corresponden a la longitud de un triangulo escaleno.
+
+
+Auxiliar:
+
+triangle(A, B, C)
+  es cierto si A, B y C corresponden a las longitudes de los lados de un triangulo
+
+ */
+
+
+ 
+ 
