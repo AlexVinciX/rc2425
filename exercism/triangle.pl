1c6f90560457c5607ff087c8b62c4cfd09dedf77
AlexVinciX <alejandro.hinestrosa@alu.uhu.es>
2025-03-07 13:57:52 +0100

    Update triangle.pl

--- exercism/triangle.pl
+++ exercism/triangle.pl
@@ -1,6 +1,6 @@
 /* 
 triangle(A, B, C, R).
- es cierto si R unicifa con "equilateral" y A, B y C corresponden a la longitud de un triangulo equilatero.
+ es cierto si R unifica con "equilateral" y A, B y C corresponden a la longitud de un triangulo equilatero.
  es cierto si R unifica con "isosceles" y A, B y C corresponden a la longitud de un triangulo isosceles.
  es cierto si R unifica con "escaleno" y A, B y C corresponden a la longitud de un triangulo escaleno.
 
@@ -12,6 +12,17 @@ triangle(A, B, C)
 
  */
 
+triangle(A, B, C, R):- comprobaciontriangulo(A, B, C), triangle2(A, B, C ,R).
+// Es un triangulo si se cumplen las 3 condiciones
+comprobaciontriangulo(A, B, C):- L1 is A + B, L1 >= C, L2 is B + C, L2 >= A, L3 is A + C, L3 >= B, A > 0, B > 0, C > 0.
+
+
+
+triangle2(A, B, C, "equilateral"):- A = B, A = C.
+triangle2(A, B,C, "isosceles"):- A = B, A /= C.
+triangle2(A, B, C, "isosceles"):- A = C, A /= B.
+triangle2 (A, B, C, "isosceles"):- B = C, B /= A.
+triangle2(A, B, C, "scalene"):- A /= B, A /= C, B /= C.
 
  
  
