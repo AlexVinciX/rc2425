7a5441d569f23f78e3bc72d46da1b3823381ba62
AlexVinciX <alejandro.hinestrosa@alu.uhu.es>
2025-03-07 14:12:22 +0100

    Update triangle.pl

--- exercism/triangle.pl
+++ exercism/triangle.pl
@@ -13,16 +13,15 @@ triangle(A, B, C)
  */
 
 triangle(A, B, C, R):- comprobaciontriangulo(A, B, C), triangle2(A, B, C ,R).
-/* Es un triangulo si se cumplen las 3 condiciones */
+
 comprobaciontriangulo(A, B, C):- L1 is A + B, L1 >= C, L2 is B + C, L2 >= A, L3 is A + C, L3 >= B, A > 0, B > 0, C > 0.
 
 
 
 triangle2(A, B, C, "equilateral"):- A = B, A = C.
-triangle2(A, B,C, "isosceles"):- A = B, A /= C.
-triangle2(A, B, C, "isosceles"):- A = C, A /= B.
-triangle2 (A, B, C, "isosceles"):- B = C, B /= A.
-triangle2(A, B, C, "scalene"):- A /= B, A /= C, B /= C.
-
+triangle2(A, B,C, "isosceles"):- A = B, A \= C.
+triangle2(A, B, C, "isosceles"):- A = C, A \= B.
+triangle2(A, B, C, "isosceles"):- B = C, B \= A.
+triangle2(A, B, C, "scalene"):- A \= B, A \= C, B \= C.
  
  
