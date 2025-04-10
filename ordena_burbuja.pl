99e5e21d96bf821863404c911900446b80d6cfd9
AlexVinciX <alejandro.hinestrosa@alu.uhu.es>
2025-04-10 09:03:19 +0200

    Create ordena_burbuja.pl

---
+++ ordena_burbuja.pl
@@ -0,0 +1,20 @@
+/*
+ordena_burbuja(+Lista,- ListaR)
+es cierto cuando ListaR unifica con una lista que contiene los mismos elementos que Lista ordenados de menor a mayor.
+
+*/
+
+/*
+ordenada(Lista)
+  es cierto si los elementos de la Lista están ordenados de menor a mayor.
+
+Principio de Inducción
+1) P(n0)
+2) P(n-1) -> P(n)
+   P(N):- N2 is N - 1, P(N2).
+
+*/
+
+ordenada([]).
+ordenada([_]).
+ordenada([Cab1, Cab2|Resto]):- Cab1 < Cab2, ordenada(Cab2|Resto).
