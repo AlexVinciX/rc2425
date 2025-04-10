d79e7fb17a48403029a12e7fd919e2918807d89a
AlexVinciX <alejandro.hinestrosa@alu.uhu.es>
2025-04-10 09:06:53 +0200

    Update ordena_burbuja.pl

--- ordena_burbuja.pl
+++ ordena_burbuja.pl
@@ -17,4 +17,4 @@ Principio de Inducción
 
 ordenada([]).
 ordenada([_]).
-ordenada([Cab1, Cab2|Resto]):- Cab1 < Cab2, ordenada(Cab2|Resto).
+ordenada([Cab1, Cab2|Resto]) :- Cab1 < Cab2, ordenada([Cab2|Resto]).
