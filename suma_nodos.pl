ca6b09d889e35595f90a9d22852b8034c26ca792
AlexVinciX <alejandro.hinestrosa@alu.uhu.es>
2025-05-09 10:19:19 +0200

    Create suma_nodos.pl

---
+++ suma_nodos.pl
@@ -0,0 +1,41 @@
+/*
+
+   suma_nodos(ArbolBinario, R)
+      es cierto si R unifica con la suma de todas las etiquetas de ArbolBinario.
+
+*/
+
+suma_nodos([], 0).
+suma_nodos(
+
+
+/*
+
+pertenece(Elem, ArbolBinario)
+    es cierto si Elem es una etiqueta de ArbolBinario
+
+*/
+
+
+pertenece(_,[]).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pertenece(Elem, ) :- pertenece(Elem,Izq), pertenece(Elem,Der).
